f6ef60d4b0996821df20b1bb7eef1f1131f11e3e
Salum Nassor <salim.nassor.sn@gmail.com>
2025-01-30 20:14:40 +0300

    feat: Added project primitives

---
+++ src/main/kotlin/com/example/projectmanagement/common/domain/enumarations/ProjectChangeType.kt
@@ -0,0 +1,9 @@
+package com.example.projectmanagement.common.domain.enumarations
+
+enum class ProjectChangeType(
+    value: String
+) {
+    CREATED("CREATED"),
+    UPDATED("UPDATED"),
+    DELETED("DELETED"),
+}
